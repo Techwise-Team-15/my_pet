9b1cf0f632c384a0774e98ce0ac4bf958f37560e
dinaolmelak <dinaolmelak@gmail.com>
2023-07-14 11:19:37 -0400

    Added pet_selection to main_menu

--- main_menu.py
+++ main_menu.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from game_util import PetConfig as config
+from pet_selection import PetSelection
 
 pygame.init()
 
@@ -73,31 +74,17 @@ class MainMenu:
             MenuItem("Options", (screen_width // 2, screen_height // 2 + 100)),
             MenuItem("Quit", (screen_width // 2, screen_height // 2 + 150)),
         ]
+        self.select_option = None
 
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
             for item in self.menu_items:
                 if item.is_mouse_selection(mouse_pos):
-                    self.select_option(item.text)
-
-    def select_option(self, option):
-        if option == "Start Game":
-            print("Start Game clicked!")
-            # Start code goes here
-
-        elif option == "Load Game":
-            print("Load Game clicked!")
-            # Load game code goes here
-
-        elif option == "Options":
-            print("Options clicked!")
-            # Options menu code goes here
-
-        elif option == "Quit":
-            print("Quit clicked!")
-            pygame.quit()
-            sys.exit()
+                    self.select_option = item.text
+                    return
+        self.select_option = None
+    
 
     def draw(self):
         self.screen.fill(BLACK)
@@ -120,7 +107,9 @@ class Game:
         pygame.display.set_caption("Pet Patrol")
         self.start_screen = StartScreen()
         self.main_menu = MainMenu()
-        self.show_start_screen = True
+        self.my_pet_screen = PetSelection(pygame, screen)
+        self.my_pet_screen.initialize_pets()
+        self.current_screen = "start"
 
     def run(self):
         running = True
@@ -130,15 +119,24 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
-                if self.show_start_screen:
+                if self.current_screen == "start":
                     transition_to_menu = self.start_screen.handle_event(event)
                     if transition_to_menu:
-                        self.show_start_screen = False
+                        self.current_screen = "menu"
+
+                elif self.current_screen == "menu":
+                    self.main_menu.handle_event(event)
+                    if self.main_menu.select_option == "Choose Your Pet":
+                        self.current_screen = "pet_selection"
                 else:
                     self.main_menu.handle_event(event)
 
-            if self.show_start_screen:
+            if self.current_screen == "start":
                 self.start_screen.draw()
+            elif self.current_screen == "menu":
+                self.main_menu.draw()
+            elif self.current_screen == "pet_selection":
+                self.my_pet_screen.main_frames()
             else:
                 self.main_menu.draw()
 
--- pet_selection.py
+++ pet_selection.py
@@ -5,72 +5,42 @@ from assets import Table
 
 
 class PetSelection():
-    def __init__(self) -> None:
-        self.pet_pygame = pygame
-        self.pet_pygame.init()
-        self.screen = self.pet_pygame.display.set_mode((Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT))
+    def __init__(self,in_pygame,in_screen) -> None:
+        self.pet_pygame = in_pygame
+        self.screen = in_screen
         self.background = self.pet_pygame.image.load('../my_pet/load_game/background_exp.PNG')
         self.bg = self.pet_pygame.transform.scale(self.background, [Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT])
-        self.pet_pygame.display.set_caption('SpriteSheets')
+        self.pet_pygame.display.set_caption('Pet Selection')
         self.last_update = self.pet_pygame.time.get_ticks()
         self.animation_cooldown = 100
-        self.run = True
+        self.pets_to_display = []
 
-
+    def add_pet(self, pet):
+        self.pets_to_display.append(pet)
     
-    def create_pet_screen(self, action):
+    def initialize_pets(self):
+        
         # Create the raccoon
-        My_Raccoon = PetRaccoon(input_pygame= self.pet_pygame, screen=self.screen)
-        My_Raccoon.set_current_animation(action)
-        My_Raccoon.set_location(100,400)
-        #raccoon_animation_list = My_Raccoon.get_animation_lists(action)
+        My_Raccoon = PetRaccoon(input_pygame=self.pet_pygame, screen=self.screen)
+        My_Raccoon.set_current_animation(Config.RaccoonActions.idle.value)
+        My_Raccoon.set_location(100, 400)
+        self.add_pet(My_Raccoon)
+
         # Create the rock
-        My_rock = PetRock(input_pygame= self.pet_pygame, screen=self.screen)
-        My_rock.set_location(600,600)
-        My_rock.set_current_animation(action)
-        #rock_animation_list = My_rock.get_animation_lists(action)
+        My_rock = PetRock(input_pygame=self.pet_pygame, screen=self.screen)
+        My_rock.set_location(600, 600)
+        My_rock.set_current_animation(Config.RockActions.jumping.value)
+        self.add_pet(My_rock)
+
         # Create the mudskipper
-        My_mudskipper = PetMudskipper(input_pygame= self.pet_pygame, screen=self.screen)
-        My_mudskipper.set_current_animation(action)
-        My_mudskipper.set_location(1100,400)
-        
-        #my_table = Table(my_pygame=self.pet_pygame, screen=self.screen, x=800, y=200)
-        #my_table.set_current_selected_animation(Config.TableActions.lamp.value) # lamp and vase are the only two options
-
-        
-        while self.run:
-            #self.screen.fill(Config.BLACK)  # Replace (0, 0, 0) with your desired background color
-            self.screen.blit(self.bg, (0,0))
-            # Animation of the pets on the screen
-            #self.screen.blit(rock_animation_list[frame], (0, 150))
-            #character_surface = self.pet_pygame.Surface((, character_image.get_height()), pygame.SRCALPHA)
-            self.screen.blit(My_rock.get_current_frame(),My_rock.get_location())
-            My_rock.updated_frame()
-            self.screen.blit(My_Raccoon.get_current_frame(), My_Raccoon.get_location())
-            My_Raccoon.updated_frame()
-            self.screen.blit(My_mudskipper.get_current_frame(), My_mudskipper.get_location())
-            My_mudskipper.updated_frame()
-            #self.screen.blit(my_table.get_current_frame(), my_table.get_location())
-            #my_table.update()
-            
-            #self.screen.blit(raccoon_animation_list[frame], (350, 150))
-            #self.screen.blit(mudskipper_animation_list[frame], (350, 350))
-            self.pet_pygame.display.flip()
-            for event in self.pet_pygame.event.get():
-                if event.type == self.pet_pygame.QUIT:
-                    self.run = False
-            self.pet_pygame.display.update()
-        self.pet_pygame.quit()
-
-
-my_pet_screen = PetSelection()
-my_pet_screen.create_pet_screen(Config.RaccoonActions.walking.value)
-
-#My_Raccoon = Pet_Raccoon(input_pygame= pygame, screen=screen)
-#My_Raccoon.animation(action=Config.RaccoonActions.clean.value)
-
-#My_Rock = Pet_Rock(input_pygame= pygame, screen=screen)
-#My_Rock.animation(action=Config.RockActions.clean.value)
-
-#My_Mudskipper = Pet_Mudskipper(input_pygame= pygame, screen=screen)
-#My_Mudskipper.animation(action=Config.MudskipperActions.walking.value)
+        My_mudskipper = PetMudskipper(input_pygame=self.pet_pygame, screen=self.screen)
+        My_mudskipper.set_current_animation(Config.MudskipperActions.idle.value)
+        My_mudskipper.set_location(1100, 400)
+        self.add_pet(My_mudskipper)
+    
+    def main_frames(self):
+        self.screen.blit(self.bg, (0,0))
+        for pet in self.pets_to_display:
+            self.screen.blit(pet.get_current_frame(),pet.get_location())
+            pet.updated_frame()
+        self.pet_pygame.display.flip()
