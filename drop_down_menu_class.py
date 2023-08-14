09447e0e7b864926a517c5c9edf5e4ea9bc29aff
dinaolmelak <dinaolmelak@gmail.com>
2023-08-13 20:50:36 -0400

    Added drop_down_menu_class.py to main menu

--- drop_down_menu_class.py
+++ drop_down_menu_class.py
@@ -1,14 +1,11 @@
 import pygame
 import random
+from game_util import PetConfig as config
 
 class PetNameSelectionApp:
-    def __init__(self):
+    def __init__(self, screen):
         pygame.init()
-
-        self.SCREEN_WIDTH = 800
-        self.SCREEN_HEIGHT = 600
-
-        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        self.screen = screen
         pygame.display.set_caption("Pet Name Selection")
 
         self.SCREEN_MAIN = 0
@@ -18,18 +15,14 @@ class PetNameSelectionApp:
         self.pet_name = self.generate_random_name()
 
         self.background_image = pygame.image.load('theme_items/general_background1.png').convert_alpha()
-        self.background_image_scale = pygame.transform.scale(self.background_image, (self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
-
-        self.WHITE = (255, 255, 255)
-        self.BLACK = (0, 0, 0)
-        self.BLUE = (0, 0, 255)
+        self.background_image_scale = pygame.transform.scale(self.background_image, (config.SCREEN_WIDTH, config.SCREEN_HEIGHT))
 
         self.font = pygame.font.Font(None, 30)
         self.input_box_font = pygame.font.Font(None, 24)
         self.notification_font = pygame.font.Font(None, 20)
 
-        self.screen_center_x = self.SCREEN_WIDTH // 2
-        self.screen_center_y = self.SCREEN_HEIGHT // 2
+        self.screen_center_x = config.SCREEN_WIDTH // 2
+        self.screen_center_y = config.SCREEN_HEIGHT // 2
 
         self.dropdown_width = 200
         self.dropdown_height = 30
@@ -58,8 +51,6 @@ class PetNameSelectionApp:
         self.delete_button_height = 30
         self.delete_buttons_rect = []
 
-        self.running = True
-
     def generate_random_name(self):
         return random.choice(self.pet_names)
 
@@ -73,7 +64,8 @@ class PetNameSelectionApp:
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.running = False
+                pygame.quit()
+                exit()
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 self.handle_mouse_click(event.pos)
             elif event.type == pygame.KEYDOWN:
@@ -135,7 +127,7 @@ class PetNameSelectionApp:
     def handle_input_box_return(self):
         if self.input_box_text.strip() != "":
             pet_name = self.input_box_text.strip()
-            self.handle_pet_name_selection(len(self.pet_names))  # Add the new custom name
+            self.handle_pet_name_selection(len(pet_name))  # Add the new custom name
             self.input_box_active = False
 
     def handle_dropdown_options_hover(self):
@@ -143,88 +135,84 @@ class PetNameSelectionApp:
             if option_rect.collidepoint(pygame.mouse.get_pos()):
                 pygame.draw.rect(self.screen, (0, 0, 150), option_rect)
 
-    def run(self):
-        while self.running:
-            self.handle_events()
-            self.draw()
-            pygame.display.flip()
-
-        pygame.quit()
-
     def draw(self):
         self.screen.blit(self.background_image_scale, (0, 0))
 
         if self.current_screen == self.SCREEN_MAIN:
-            pygame.draw.rect(self.screen, self.WHITE, self.dropdown_rect, 2)
-            dropdown_text = self.font.render(self.pet_name, True, self.BLACK)
+            pygame.draw.rect(self.screen, config.WHITE, self.dropdown_rect, 2)
+            dropdown_text = self.font.render(self.pet_name, True, config.BLACK)
             dropdown_text_rect = dropdown_text.get_rect(center=self.dropdown_rect.center)
             self.screen.blit(dropdown_text, dropdown_text_rect)
 
-            choose_text = self.font.render("Choose Your Pet Name", True, self.BLACK)
+            choose_text = self.font.render("Choose Your Pet Name", True, config.BLACK)
             choose_text_rect = choose_text.get_rect(center=(self.screen_center_x, self.dropdown_rect.top - 20))
             self.screen.blit(choose_text, choose_text_rect)
 
             if self.is_dropdown_open:
                 for i, option_rect in enumerate(self.dropdown_options_rect):
-                    pygame.draw.rect(self.screen, self.BLUE, option_rect)
-                    option_text = self.font.render(self.pet_names[i], True, self.WHITE)
+                    pygame.draw.rect(self.screen, config.BLUE, option_rect)
+                    option_text = self.font.render(self.pet_names[i], True, config.WHITE)
                     option_text_rect = option_text.get_rect(center=option_rect.center)
                     self.screen.blit(option_text, option_text_rect)
 
-            self.save_button_rect = pygame.Rect(self.SCREEN_WIDTH // 2 - 50, self.screen_center_y + 150, 100, 30)
-            pygame.draw.rect(self.screen, self.BLUE, self.save_button_rect)
-            save_button_text = self.font.render("Save", True, self.WHITE)
+            self.save_button_rect = pygame.Rect(config.SCREEN_WIDTH // 2 - 50, self.screen_center_y + 150, 100, 30)
+            pygame.draw.rect(self.screen, config.BLUE, self.save_button_rect)
+            save_button_text = self.font.render("Save", True, config.WHITE)
             save_button_text_rect = save_button_text.get_rect(center=self.save_button_rect.center)
             self.screen.blit(save_button_text, save_button_text_rect)
 
         elif self.current_screen == self.SCREEN_SAVED_NAMES:
-            saved_screen_text = self.font.render("Saved Pet Names:", True, self.BLACK)
+            saved_screen_text = self.font.render("Saved Pet Names:", True, config.BLACK)
             self.screen.blit(saved_screen_text, (self.screen_center_x - saved_screen_text.get_width() // 2, 50))
 
             self.delete_buttons_rect = []
             for i, name in enumerate(self.saved_names[:self.max_saved_names]):
-                name_text = self.font.render(name, True, self.BLACK)
+                name_text = self.font.render(name, True, config.BLACK)
                 name_text_rect = name_text.get_rect(center=(self.screen_center_x, 100 + i * 30))
                 self.screen.blit(name_text, name_text_rect)
 
                 delete_button_position = (name_text_rect.right + 10, name_text_rect.centery - self.delete_button_height // 2)
                 delete_button_rect = pygame.Rect(delete_button_position[0], delete_button_position[1], self.delete_button_width, self.delete_button_height)
-                pygame.draw.rect(self.screen, self.BLUE, delete_button_rect)
-                delete_button_text = self.font.render("Delete", True, self.WHITE)
+                pygame.draw.rect(self.screen, config.BLUE, delete_button_rect)
+                delete_button_text = self.font.render("Delete", True, config.WHITE)
                 delete_button_text_rect = delete_button_text.get_rect(center=delete_button_rect.center)
                 self.screen.blit(delete_button_text, delete_button_text_rect)
 
                 self.delete_buttons_rect.append(delete_button_rect)
 
-            go_back_button_position = (self.SCREEN_WIDTH // 2 - 60, self.screen_center_y + 150)
+            go_back_button_position = (config.SCREEN_WIDTH // 2 - 60, self.screen_center_y + 150)
             self.go_back_button_rect = pygame.Rect(go_back_button_position[0], go_back_button_position[1], 120, 30)
-            pygame.draw.rect(self.screen, self.BLUE, self.go_back_button_rect)
-            go_back_button_text = self.font.render("Go Back", True, self.WHITE)
+            pygame.draw.rect(self.screen, config.BLUE, self.go_back_button_rect)
+            go_back_button_text = self.font.render("Go Back", True, config.WHITE)
             go_back_button_text_rect = go_back_button_text.get_rect(center=self.go_back_button_rect.center)
             self.screen.blit(go_back_button_text, go_back_button_text_rect)
 
         if self.input_box_active and not self.show_save_screen:
-            pygame.draw.rect(self.screen, self.WHITE, self.input_box_rect, 2)
-            input_box_surface = self.input_box_font.render(self.input_box_text, True, self.BLACK)
+            pygame.draw.rect(self.screen, config.WHITE, self.input_box_rect, 2)
+            input_box_surface = self.input_box_font.render(self.input_box_text, True, config.BLACK)
             input_box_surface_rect = input_box_surface.get_rect(center=self.input_box_rect.center)
             self.screen.blit(input_box_surface, input_box_surface_rect)
 
-            pygame.draw.rect(self.screen, self.BLUE, self.input_box_rect, 2)
+            pygame.draw.rect(self.screen, config.BLUE, self.input_box_rect, 2)
 
-            custom_name_label = self.font.render("Enter Custom Name:", True, self.BLACK)
+            custom_name_label = self.font.render("Enter Custom Name:", True, config.BLACK)
             custom_name_label_rect = custom_name_label.get_rect(center=(self.screen_center_x, self.screen_center_y + 100))
             self.screen.blit(custom_name_label, custom_name_label_rect)
 
         if self.show_notification:
-            notification_surface = self.notification_font.render(self.notification_text, True, self.BLACK)
+            notification_surface = self.notification_font.render(self.notification_text, True, config.BLACK)
             notification_rect = notification_surface.get_rect(center=(self.screen_center_x, self.screen_center_y + 100))
             self.screen.blit(notification_surface, notification_rect)
             self.show_notification = False
             pygame.time.set_timer(pygame.USEREVENT, 2000)
+    
+    def main_frames(self):
+        # while self.running:
+        self.draw()
+        self.handle_events()
+        
+        
 
-
-if __name__ == "__main__":
-    app = PetNameSelectionApp()
-    app.run()
+        # pygame.quit()
 
 
